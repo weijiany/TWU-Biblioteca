f8cabf8aacc62591b6656fcb948289a989fc6fba
weijian <weijian.yang@thoughtworks.com>
2020-02-16 09:41:52 +0800

    #1.6 exit app

--- src/main/java/com/twu/BibliotecaApplication.java
+++ src/main/java/com/twu/BibliotecaApplication.java
@@ -2,15 +2,16 @@ package com.twu;
 
 import com.twu.model.BooksList;
 import com.twu.model.exception.DomainException;
-import com.twu.model.exception.NotAVailableException;
 
 public class BibliotecaApplication {
 
     public final static String LIST_OF_BOOKS = "List of books.";
     public final static String CHECK_OUT_BOOK = "Check-out a book.";
     public final static String RETURN_BOOK = "Return a book.";
+    public final static String EXIT = "Exit";
     public final static String MENU = "================MENU================\n1. " +
-            LIST_OF_BOOKS + "\n2." + CHECK_OUT_BOOK + "\n3." + RETURN_BOOK;
+            LIST_OF_BOOKS + "\n2." + CHECK_OUT_BOOK + "\n3." + RETURN_BOOK +
+            "\n4" + EXIT;
     public final static String WELCOME = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
     public static final String CHECK_OUT_SUCCESSFULLY = "Thank you! Enjoy the book.";
     public static final String RETURN_SUCCESSFULLY = "Thank you for returning the book.";
@@ -41,6 +42,8 @@ public class BibliotecaApplication {
                     booksList.returnABook(ConsoleUtil.next());
                     ConsoleUtil.println(RETURN_SUCCESSFULLY);
                     break;
+                case "4":
+                    System.exit(0);
                 default:
                     ConsoleUtil.println(SELECT_ERR_OPTION);
             }
